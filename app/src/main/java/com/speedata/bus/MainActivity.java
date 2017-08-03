81b7ac93b93771267f7a811482b7a7f543e6ef0f
Reginer <282921012@qq.com>
2017-08-04 01:46:43 +0800

    去0，去1，起止时间获取

--- app/src/main/java/com/speedata/bus/MainActivity.java
+++ app/src/main/java/com/speedata/bus/MainActivity.java
@@ -11,6 +11,7 @@ import java.util.Arrays;
 public class MainActivity extends AppCompatActivity {
     String content = "RfQGDEXENzVKalY52MqdYKkL5OCh4E+Io+y39x86FjWaNoxpyqR8ynTkbbI4hr1yeN0lxgVIp7XmVsH42nYNZP+11Ho0G1I8MRJSqBLYIiOEPbBwPnwkDVBcCatH/3K6GbcyAB/+AE9o5w0ziCstPkg8XAAAW/Jpza217OE5BUodSCgizJo/iEf0Bgw=";
     String privateKey = "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBALszO9xwPNFcjT7ObmPucUPDixGtUItErqNIvlyJamRIOOW4Nx9tEu3IVZJkoIg9y4o0Cclv4E4Rn09jzgOGqC5y5SkX6YVX3v1yYnMRXlAj1nXmGYvdzy5pK2dXBvj9MqVvyREgApJRf5WFT/mCoM8oamQzbj/DzowKa/ZMMysTAgMBAAECgYAX8T+QvBIrPMkb5U8dHNwDrLjWs/H/PfOQEre2IiUnWk1cpCj4rfyjyOGY7aHSiwTDUlCGIPbxRTO49azdqpVL03itVCKpNWHRm1oyT87uMiZjRNincLFpecZuL5grFx7uk3XXDJNJnMVeRzIAeA4648Hz1kGnL7rSPl1nZMoaoQJBAOYoV4jGWlYnFNitgUnS3cT8xMyg0iFY+sB4R0UA8OM2PPooxQ8zSt7sUpNjJZeKr+C+vvTI0S2lZh3E1vE14msCQQDQOB8MiqnA0QtxdX07gQBbKuOjhaqbgwYtQNbz9bOVfuQkINyjGAYGjNDTAif3lk6esD02qO5E+xeYPxrj9BP5AkEA3+8+g/iiKpLp6Iygb8i4OBLMFGEhMosVprEgftYUiq3aFHgYi9qXLWD4UykziUfKKjzNJPRC+kukmLBNhfGsswJBAIEJJoS5MfXiFrtbWh6UFUpqS+Q8o7v3+M32jr9lFsOgfXW+P71619EbVxZNEcOnjFVfVrAXBIIPMVISWXHHAtECQCYuq87SlClUON9gok+q4Cys1Koqi11WyLLCMB3Pp1cV8CXRRfGn5yWFTW3VhOEJUQZ08NO30V4oKptfb2k3sn0=";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,14 +46,23 @@ public class MainActivity extends AppCompatActivity {
         }
         long userId = ByteUtils.byteToLong(ByteUtils.sub(decodeRSA, 0, 8));
         System.out.println(userId);
-        long time = ByteUtils.byteToLong(ByteUtils.sub(decodeRSA, 8, 16));
+        long time = ByteUtils.byteToLong(ByteUtils.sub(decodeRSA, 8, 8));
         System.out.println(time);
-
-
+        byte[] startTime = ByteUtils.sub(decodeRSA, 17, 1);
+        System.out.println("start time is::" + Arrays.toString(startTime));
+        byte[] endTime = ByteUtils.sub(decodeRSA, 19, 1);
+        System.out.println("end time is::" + Arrays.toString(endTime));
+        byte[] accountFlow = ByteUtils.sub(decodeRSA, 21, 1);
+        System.out.println("accountFlow  is::" + Arrays.toString(accountFlow));
     }
 
     private static byte[] getFinalByte(byte[] convertBytes) {
-
-        return Arrays.copyOfRange(convertBytes, 0, convertBytes.length - 4);
+        int length = 0;
+        for (int i = convertBytes.length - 1; i >= 0; i--) {
+            if (convertBytes[i] == 0 && convertBytes[i - 1] == 1) {
+                length = i;
+            }
+        }
+        return Arrays.copyOfRange(convertBytes, 0, length - 1);
     }
 }
